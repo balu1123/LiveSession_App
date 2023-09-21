3ceb251a4e3d174905ad3eb9f89d2e964d4dec1e
balu1123 <90262406+balu1123@users.noreply.github.com>
2023-09-21 12:48:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import svgrPlugin from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
+// see all documentation here https://vitejs.dev/config/
 export default defineConfig({
+  // This changes the out put dir from dist to build change as your need
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
   server: {
     host: true,
     port: 80,
   },
-  plugins: [react()],
+  plugins: [
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
 })
